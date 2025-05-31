8cbfd2366b71e457692e8957b23f1fede2918717
varaujod <varaujod08@gmail.com>
2025-05-31 15:24:54 -0300

    Alteração em Service de Estoque

--- src/service/EstoqueService.ts
+++ src/service/EstoqueService.ts
@@ -1,12 +1,26 @@
 import { EstoqueEntity } from "../model/EstoqueEntity";
 import { EstoqueRepository } from "../repository/EstoqueRepository";
+import { LivroRepository } from "../repository/LivroRepository";
 
 export class EstoqueService{
     private estoqueRepository = EstoqueRepository.getInstance();
+    private livroRepository = LivroRepository.getInstance();
 
     novoLivronoEstoque(data: any): EstoqueEntity{
         if(!data.isbn || !data.cod){
             throw new Error("Por favor informar todos os campos");
+        }
+
+        const livroExistente = this.livroRepository.filtraLivroPorISBN(data.isbn);
+
+        if(!livroExistente) {
+            throw new Error("Não é possível adicionar um exemplar de um livro que não está cadastrado. Por favor, cadastre o livro primeiro.");
+        }
+
+        const exemplarExistente = this.estoqueRepository.filtraLivroNoEstoque(data.cod);
+
+        if(exemplarExistente) {
+            throw new Error("Já existe um exemplar com este código!");
         }
 
         const livroNoEstoque = new EstoqueEntity(data.isbn, data.cod);
@@ -22,7 +36,18 @@ export class EstoqueService{
 
     filtrarLivroNoEstoque(data: any){
         const cod = data.cod;
-        return this.estoqueRepository.filtraLivroNoEstoque(cod);
+        const exemplar = this.estoqueRepository.filtraLivroNoEstoque(cod);
+        
+        if(!exemplar) {
+            throw new Error("Exemplar não encontrado");
+        }
+
+        const livro = this.livroRepository.filtraLivroPorISBN(exemplar.isbn);
+        
+        return {
+            exemplar: exemplar,
+            livro: livro
+        };
     }
 
     atualizarDisponibilidade(data: any){
